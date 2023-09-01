98d51d5ccfe2dba98b81a83ee573fb281aead893
Olzhas <131268326+olzhassky@users.noreply.github.com>
2023-09-01 18:32:16 +0600

    asd

--- blackListTests/blackListTests.swift
+++ blackListTests/blackListTests.swift
@@ -6,31 +6,34 @@
 //
 
 import XCTest
+import RealmSwift
+
 @testable import blackList
 
 final class blackListTests: XCTestCase {
-
+    var table: TableViewController!
+    var menuVC: MenuViewController!
+    
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        table = TableViewController()
+        menuVC = MenuViewController()
+        
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        table = nil
+        menuVC = nil
     }
 
     func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+        let product = ProductClass()
+        XCTAssertEqual(product.name, "")
+        XCTAssertEqual(product.amount, 0)
+        XCTAssertEqual(product.selected, false)
     }
+    func textHomeButton() {
+        menuVC.homeButton.sendActions(for: .touchUpInside)
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
     }
-
+    
 }
